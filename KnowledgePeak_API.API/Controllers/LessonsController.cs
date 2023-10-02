d55956584ec260ab680c13fcfea0b3eace31c6c9
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-10-02 13:06:33 +0400

    Complete LEsson Controller

---
+++ KnowledgePeak_API.API/Controllers/LessonsController.cs
@@ -0,0 +1,64 @@
+﻿using KnowledgePeak_API.Business.Dtos.LessonDtos;
+using KnowledgePeak_API.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowledgePeak_API.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LessonsController : ControllerBase
+{
+    readonly ILessonService _service;
+
+    public LessonsController(ILessonService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _service.GetAllAsync(true));
+    }
+
+    [HttpGet("[action]/{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await _service.GetByIdAsync(id, true));
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create([FromForm] LessonCreateDto dto)
+    {
+        await _service.CreateAsync(dto);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+
+    [HttpPut("[action]/{id}")]
+    public async Task<IActionResult> Update([FromForm] LessonUpdateDto dto, int id)
+    {
+        await _service.UpdateAsync(id, dto);
+        return StatusCode(StatusCodes.Status200OK);
+    }
+
+    [HttpDelete("[action]/{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _service.DeleteAsync(id);
+        return StatusCode(StatusCodes.Status200OK);
+    }
+
+    [HttpPatch("[action]/{id}")]
+    public async Task<IActionResult> SoftDelete(int id)
+    {
+        await _service.SoftDeleteAsync(id);
+        return StatusCode(StatusCodes.Status200OK);
+    }
+
+    [HttpPatch("[action]/{id}")]
+    public async Task<IActionResult> RevertSoftDelete(int id)
+    {
+        await _service.RevertSoftDeleteAsync(id);
+        return StatusCode(StatusCodes.Status200OK);
+    }
+}
